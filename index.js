01cf6d611afb4efe53bbf3d0f2b3f9a075215eac
MAwais6 <f200265@cfd.nu.edu.pk>
2023-03-25 18:17:39 +0500

    Convert Simple API to Schedule API

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
+const schedule = require("node-schedule");
+
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
+  // Schedule a job to run every 5 seconds 
+    const job = schedule.scheduleJob("*/5 * * * * *", () => {
+        console.log("This job runs after 5 seconds. and then cancel it");
+        job.cancel();
+    });
 });
-app.listen(5000, () => {
+
+app.listen(3000, () => {
   console.log("Running on port 5000.");
 });
+
 
 // Export the Express API
 module.exports = app;
